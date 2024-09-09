da089ca8163eeccd33a64b9f5019c388d3630f25
orechen422dazo <jboy422hobby@gmail.com>
2024-09-10 00:38:31 +0900

    wip

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by 橋本純一 on 2024/09/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
